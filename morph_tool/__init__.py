83ff54b525ff14cb38cabf74107fe8ec86393b70
Benoît Coste <benoit.coste@epfl.ch>
2020-03-25 11:11:12 +0100

    fixup! Use setuptools_scm

--- morph_tool/__init__.py
+++ morph_tool/__init__.py
@@ -5,7 +5,6 @@ from morph_tool.apical_point import apical_point_position, apical_point_section_
 from morph_tool.converter import convert
 from morph_tool.morphio_diff import diff
 from morph_tool.spatial import point_to_section_segment
-from morph_tool.version import __version__
 
 
 class MorphToolException(Exception):
