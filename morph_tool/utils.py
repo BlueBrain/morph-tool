3cc67f492c7b2717cf2d33f954b4f943bca86c44
Benoît Coste <benoit.coste@epfl.ch>
2020-07-14 15:25:07 +0200

    Improve neurondb_dataframe
    For a better whitespace handling in the DAT file and with the NA check disabled for
    better performances

--- morph_tool/utils.py
+++ morph_tool/utils.py
@@ -27,6 +27,7 @@ def neurondb_dataframe(filename: Path) -> pd.DataFrame:
     Args:
         filename: the neurondb.dat file
     '''
-    df = pd.read_csv(filename, sep=' ', names=['name', 'layer', 'mtype'])
+    df = pd.read_csv(filename, sep=r'\s+', names=['name', 'layer', 'mtype'],
+                     na_filter=False)
     df.layer = df.layer.astype('str')
     return df
