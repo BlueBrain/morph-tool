92fda6878fef1c4f2400c98f231ec28ecb7182ec
Benoit Coste <benoit.coste@protonmail.com>
2020-07-17 09:52:09 +0200

    neurondb_dataframe can take as input an XML file (#24)
    It is no longer dedicated to dat files

--- morph_tool/utils.py
+++ morph_tool/utils.py
@@ -3,6 +3,7 @@ from functools import partial
 from pathlib import Path
 
 import pandas as pd
+import xmltodict
 
 
 def is_morphology(filename, extensions=None):
@@ -24,10 +25,32 @@ def iter_morphology_files(folder, recursive=False, extensions=None):
 def neurondb_dataframe(filename: Path) -> pd.DataFrame:
     '''Returns a DataFrame: [name, layer, mtype]
 
+    If read from an XML, additional columns maybe be present
     Args:
-        filename: the neurondb.dat file
+        filename: the neurondb.(dat|xml) file
     '''
-    columns = ['name', 'layer', 'mtype']
-    df = pd.read_csv(filename, sep=r'\s+', names=columns, usecols=range(len(columns)))
-    df.layer = df.layer.astype('str')
+    if filename.suffix.lower() == '.dat':
+        columns = ['name', 'layer', 'mtype']
+        df = pd.read_csv(filename, sep=r'\s+', names=columns, usecols=range(len(columns)))
+        df.layer = df.layer.astype('str')
+    elif filename.suffix.lower() == '.xml':
+        with filename.open() as fd:
+            neuronDB = xmltodict.parse(fd.read())
+
+        rows = list()
+        for morph in neuronDB["neurondb"]["listing"]["morphology"]:
+            assert morph["repair"]["use_axon"] in {'true', 'false', 'True', 'False', None}
+            rows.append(
+                (morph["name"],
+                 morph["layer"],
+                 morph["mtype"] + (":" + morph["msubtype"] if morph["msubtype"] else ""),
+                 # According to Eilif, an empty use_axon (corresponding to a null in the database)
+                 # means that the axon is supposed to be used
+                 (morph["repair"]["use_axon"] in {'true', 'True', None})))
+
+        df = pd.DataFrame(data=rows, columns=['name', 'layer', 'mtype', 'use_axon'])
+
+    else:
+        raise ValueError(f'Unrecognized extension for neurondb file: {filename}')
+
     return df
