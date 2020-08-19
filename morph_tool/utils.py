0d46687cc93f9fa3586c90f85a118e8735128b41
Benoit Coste <benoit.coste@protonmail.com>
2020-08-19 16:37:51 +0200

    neurondb_dataframe() handle morphology paths (#30)
    neurondb_dataframe accepts a new option argument morphology_dir.
    When passed, the returned dataframe will have an additional column with the path
    of the morphologies found in morphology_dir.
    
    And fix a bug when a single morphology is present in neuronDB.xml

--- morph_tool/utils.py
+++ morph_tool/utils.py
@@ -1,6 +1,7 @@
 '''Utils'''
 from functools import partial
 from pathlib import Path
+from typing import Optional
 
 import pandas as pd
 import xmltodict
@@ -22,23 +23,56 @@ def iter_morphology_files(folder, recursive=False, extensions=None):
     return filter(partial(is_morphology, extensions=extensions), files)
 
 
-def neurondb_dataframe(filename: Path) -> pd.DataFrame:
+def find_morph(folder: Path, stem: str) -> Optional[Path]:
+    '''Returns the path to a morphology in morphology_dir matching stem.
+
+    If no morphology is found, returns None
+    '''
+    for ext in {'.asc', '.ASC', '.h5', '.H5', '.swc', '.SWC'}:
+        path = folder / (stem + ext)
+        if path.exists():
+            return path
+    return None
+
+
+def _ensure_list(data):
+    '''Returns the data if already a list else [data].
+
+    xmltodict.parse will represent the data as a list if it sees
+    the same tag multiple times in a row. For example,
+    <morphology></morphology> will appear multiple times in neuronDB.xml
+
+    However if the neurondb has a single <morphology></morphology> tag the data
+    won't be represented as a list so this function enforce it
+
+    Args:
+        data: the python dict coming from xmltodict.parse.
+
+    '''
+    if isinstance(data, list):
+        return data
+    return [data]
+
+
+def neurondb_dataframe(neurondb: Path, morphology_dir: Optional[Path] = None) -> pd.DataFrame:
     '''Returns a DataFrame: [name, layer, mtype]
 
     If read from an XML, additional columns maybe be present
     Args:
         filename: the neurondb.(dat|xml) file
+        morphology_dir: (Optional) If passed, a column with the path to each morphology file
+            will be added
     '''
-    if filename.suffix.lower() == '.dat':
+    if neurondb.suffix.lower() == '.dat':
         columns = ['name', 'layer', 'mtype']
-        df = pd.read_csv(filename, sep=r'\s+', names=columns, usecols=range(len(columns)))
+        df = pd.read_csv(neurondb, sep=r'\s+', names=columns, usecols=range(len(columns)))
         df.layer = df.layer.astype('str')
-    elif filename.suffix.lower() == '.xml':
-        with filename.open() as fd:
+    elif neurondb.suffix.lower() == '.xml':
+        with neurondb.open() as fd:
             neuronDB = xmltodict.parse(fd.read())
 
         rows = list()
-        for morph in neuronDB["neurondb"]["listing"]["morphology"]:
+        for morph in _ensure_list(neuronDB["neurondb"]["listing"]["morphology"]):
             assert morph["repair"]["use_axon"] in {'true', 'false', 'True', 'False', None}
             rows.append(
                 (morph["name"],
@@ -51,6 +85,9 @@ def neurondb_dataframe(filename: Path) -> pd.DataFrame:
         df = pd.DataFrame(data=rows, columns=['name', 'layer', 'mtype', 'use_axon'])
 
     else:
-        raise ValueError(f'Unrecognized extension for neurondb file: {filename}')
+        raise ValueError(f'Unrecognized extension for neurondb file: {neurondb}')
+
+    if morphology_dir:
+        df['path'] = df.apply(lambda row: find_morph(morphology_dir, row['name']), axis=1)
 
     return df
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,11 @@
+import sys
+from io import StringIO
 from pathlib import Path
 
 import pandas as pd
+from mock import patch
 from nose.tools import assert_equal, assert_raises, ok_
+from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal
 
 import morph_tool.utils as tested
@@ -39,6 +43,15 @@ def test_iter_morphology_files():
                   DATA / 'folder' / 'subfolder' / 'e.h5',
 })
 
+def test_find_morph():
+    folder = DATA / 'test-neurondb-with-path'
+    assert_equal(tested.find_morph(folder, 'not-here.h5'),
+                 None)
+    assert_equal(tested.find_morph(folder, 'C270106A'),
+                 folder / 'C270106A.h5')
+    assert_equal(tested.find_morph(folder, 'C270106C.wrongext'),
+                 None)
+
 
 def test_neurondb_dataframe():
     expected = pd.DataFrame(data=[['name1', '1', 'L1_mtype-submtype'],
@@ -58,3 +71,82 @@ def test_neurondb_dataframe():
     assert_frame_equal(df, expected)
 
     assert_raises(ValueError, tested.neurondb_dataframe, DATA / 'neurondb.wrongext')
+
+
+
+def mock_path_content(content):
+    class MockPathContent:
+        '''pathlib.Path mock to mock the call to Path.open()'''
+        def __enter__(self):
+            return StringIO(content)
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            pass
+    return MockPathContent
+
+
+def test_neurondb_dataframe_single_morph():
+    neurondb = DATA / 'neurondb.xml'
+
+    neurondb_template = '''
+    <neurondb>
+    <listing>
+    <morphology>
+      <name>C270106A</name>
+      <mtype>L1_DAC</mtype>
+      <msubtype></msubtype>
+      <layer>1</layer>
+      <repair>
+        <use_axon>True</use_axon>
+      </repair>
+    </morphology>
+    </listing>
+    </neurondb>
+    '''
+
+    with patch.object(Path, 'open', mock_path_content(neurondb_template)):
+        df = tested.neurondb_dataframe(neurondb)
+        expected = pd.DataFrame(data=[['C270106A', '1', 'L1_DAC', True]],
+                                columns=['name', 'layer', 'mtype', 'use_axon'])
+        assert_frame_equal(df, expected)
+
+def test_neurondb_dataframe_use_axon():
+    neurondb = DATA / 'neurondb.xml'
+
+    neurondb_template = '''
+    <neurondb>
+    <listing>
+    <morphology>
+      <name>C270106A</name>
+      <mtype>L1_DAC</mtype>
+      <msubtype></msubtype>
+      <layer>1</layer>
+      <repair>
+        <use_axon>{}</use_axon>
+      </repair>
+    </morphology>
+    </listing>
+    </neurondb>
+    '''
+
+    for use_axon in ['True', 'true', '']:
+        with patch.object(Path, 'open', mock_path_content(neurondb_template.format(use_axon))):
+            df = tested.neurondb_dataframe(neurondb)
+            assert_equal(df.loc[0, 'use_axon'], True)
+
+    for use_axon in ['False', 'false']:
+        with patch.object(Path, 'open', mock_path_content(neurondb_template.format(use_axon))):
+            df = tested.neurondb_dataframe(neurondb)
+            assert_equal(df.loc[0, 'use_axon'], False)
+
+    for use_axon in ['tRuE', 'fals', 0, 1, 'mickael jackson']:
+        with patch.object(Path, 'open', mock_path_content(neurondb_template.format(use_axon))):
+            assert_raises(AssertionError, tested.neurondb_dataframe, neurondb)
+
+
+def test_neurondb_dataframe_with_path():
+    folder = DATA / 'test-neurondb-with-path'
+    df = tested.neurondb_dataframe(DATA / 'neurondb.xml', morphology_dir=folder)
+
+    assert_array_equal([p.stem if p is not None else None for p in df.path],
+                       ['C270106A', None, 'a_neuron', None])
