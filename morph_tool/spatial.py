d02938c85374936414a24619e0950b5851a6c3b6
arnaudon <alexis.arnaudon@epfl.ch>
2021-02-17 11:46:00 +0100

    3 methods

--- morph_tool/spatial.py
+++ morph_tool/spatial.py
@@ -1,4 +1,5 @@
 '''Module for spatial related functions'''
+import logging
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from scipy.linalg import svd
@@ -9,6 +10,7 @@ from morphio import SectionType
 
 from morph_tool.transform import rotate
 
+logger = logging.getLogger('morph_tool')
 X, Y, Z = 0, 1, 2
 
 
@@ -36,28 +38,64 @@ def point_to_section_segment(neuron, point):
     raise ValueError('Cannot find point in morphology that matches: {}'.format(point))
 
 
-def align_pyr_morph(morph, direction=None):
-    """In place alignment of a pyramidal morphology such that apical tree is along 'direction'.
+def align_pyr_morph(morph, direction=None, method='whole_apical', apical_point=None):
+    """In-place alignment of a pyramidal morphology such that apical tree is along 'direction'.
 
-    The algorithm is based on SVD decomposititon of the correlation matrix obtained from all points
-    in apical neurites, giving the principal axis of apical dendrite.
-    If not apicals are present, no rotation is applied.
+    The base algorithm is based on SVD decomposition of the correlation matrix obtained from
+    points in apical neurites, giving the principal axis of apical dendrite.
+    Currently, three algorithms are implemented, differing in the choice of points:
+
+    1) with method='whole_apical': All the points in the apical dendrite are used.
+    2) with method='first_section': Only the points in the first section are used.
+    3) with method='apical_trunk': Points in section up to the apical points are used.
+
+    If no apical is present, no rotation is applied, and the identity rotation is returned.
+    If two apicals are present, the apical accessed in second by Morphio will be used.
 
     Args:
         morph (morphio.Morphology): morphology to align
-        direction (ndarray): 3 vector with for final direction, if None, [0, 1, 0] will be used
+        direction (ndarray): 3-vector for final direction, if None, [0, 1, 0] will be used
+        method (str): method for alignment.
+        apical_point (ndarray): position of apical point for method='apical_trunk',
+            if None, it will be estimated
+
+    Returns:
+        3x3 array with applied rotation matrix
     """
+    if method not in ['whole_apical', 'apical_trunk', 'first_section']:
+        raise NotImplementedError(f"Method {method} is not implementd")
+
     if direction is None:
         direction = [0.0, 1.0, 0.0]
-    apical_points = []
-    for section in morph.iter():
-        if section.type == SectionType.apical_dendrite:
-            apical_points += list(section.points)
+    apical_points = None
+    for root_section in morph.root_sections:
+        if root_section.type == SectionType.apical_dendrite:
+            if apical_points is not None:
+                logger.info('The morphology has two apical dendrites, we will use the second.')
 
-    if len(apical_points) == 0:
-        return None
+            if method == 'apical_trunk':
+                from morph_tool.apical_point import apical_point_section_segment
+                if apical_point is None:
+                    apical_secid = apical_point_section_segment(morph)[0]
+                else:
+                    from morph_tool.spatial import point_to_section_segment
 
-    apical_points = np.array(apical_points)
+                    apical_secid = point_to_section_segment(morph, apical_point)[0] - 1
+
+                _points = []
+                for section in root_section.iter():
+                    _points.append(section.points)
+                    if section.id == apical_secid:
+                        break
+                apical_points = np.vstack(_points)
+            if method == 'first_section':
+                apical_points = root_section.points
+            else:
+                apical_points = np.vstack([section.points for section in root_section.iter()])
+
+    if apical_points is None:
+        logger.info('We did not find an apical point to align the morphology')
+        return np.eye(3)
 
     # compute covariance matrix
     cov = apical_points.T.dot(apical_points) / (len(apical_points) - 1)
@@ -76,5 +114,9 @@ def align_pyr_morph(morph, direction=None):
         """Cost to find the rotation angle."""
         return np.linalg.norm(rot(angle).dot(principal_direction) - direction)
 
+    rotation_matrix = rot(minimize_scalar(cost).x)
+
     # rotate the morphology in place
-    rotate(morph, rot(minimize_scalar(cost).x))
+    rotate(morph, rotation_matrix)
+
+    return rotation_matrix
