21ca8a9b5b7387bfc5ed5a3d9ea31c6ea44f5f2a
aleksei sanin <aleksei.sanin@epfl.ch>
2021-06-10 15:54:06 +0200

    refactor get_incomplete_sections

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -53,7 +53,3 @@ def test_loader_no_cache(f_mock):
     loader.get('test')
     loader.get('test')
     nt.assert_equal(f_mock.call_count, 2)
-
-def test_get_incomplete_sections():
-    nt.assert_equal(tested.get_incomplete_sections(DATA / "simple.asc"), [])
-    nt.assert_equal(tested.get_incomplete_sections(DATA / "test_tag.asc"), [2, 5])
