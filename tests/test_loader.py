8b80da0cc027ea6e02a41f660907e39966744166
asanin-epfl <53935643+asanin-epfl@users.noreply.github.com>
2021-06-23 17:35:29 +0200

    Revert "Introduce a function to get section ids of sections with cut leaves." (#78)
    This reverts commit 9032dec0

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,12 +1,9 @@
 import nose.tools as nt
-from pathlib import Path
 
 import mock
 from mock import patch
 
 import morph_tool.loader as tested
-
-DATA = Path(__file__).absolute().parent / "data"
 
 
 def test_ensure_startswith_point():
