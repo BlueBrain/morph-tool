ddbc7f2fbf112af8c62cb84231d13e578b601447
arnaudon <alexis.arnaudon@epfl.ch>
2021-07-16 10:38:19 +0200

    correct fix

--- morph_tool/spatial.py
+++ morph_tool/spatial.py
@@ -1,31 +1,30 @@
-'''Module for spatial related functions'''
+"""Module for spatial related functions"""
 import numpy as np
 
 from neurom import COLS
 
-X, Y, Z = 0, 1, 2
 
-
-def point_to_section_segment(neuron, point):
-    '''Find section and segment that matches the point.
+def point_to_section_segment(neuron, point, rtol=1e-05, atol=1e-08):
+    """Find section and segment that matches the point.
 
     Only the first point found with the *exact* same coordinates as the point argument is considered
 
     Args:
         neuron (morphio.Morphology): neuron object
         point (point): value of the point to find in the h5 file
+        rtol, atol (floats): precission of np.isclose
 
     Returns:
         Tuple: (NeuroM/MorphIO section ID, point ID) of the point the matches the input coordinates.
         Since NeuroM v2, section ids of NeuroM and MorphIO are the same excluding soma.
-    '''
+    """
 
     for section in neuron.iter():
         points = section.points
-        offset = np.where(np.allclose(points[:, X], point[COLS.X]) &
-                          np.allclose(points[:, Y], point[COLS.Y]) &
-                          np.allclose(points[:, Z], point[COLS.Z]))
+        offset = np.where(
+            np.isclose(points[:, COLS.XYZ], point[COLS.XYZ], rtol=rtol, atol=atol).all(axis=1)
+        )
         if offset[0].size:
             return section.id, offset[0][0]
 
-    raise ValueError('Cannot find point in morphology that matches: {}'.format(point))
+    raise ValueError("Cannot find point in morphology that matches: {}".format(point))
